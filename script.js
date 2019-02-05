00804e5c8a33f4aed76fca583d7bd6cd025c2fdc
Anry <anrypwnz@gmail.com>
2019-02-05 19:00:58 +0300

    this my first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+'use strict'
+
+//let arr = ['first', 'second', 'third']
+
+//console.log(arr[1]);
+
+
+//confirm('are you here ?');
+
+
+//let answer = confirm('are you here ?');
+
+//let answer = prompt('are you 18 years old?', 'Yes');
+
+//console.log(typeof(arr));
+
+//console.log("arr" + " - object");
+
+//console.log("arr" + " - object");
+
+let incr = 10,
+    decr = 10;
+   
+
+    console.log(incr++);
+    console.log(decr--);
+    console.log(92%3);
+    
+
